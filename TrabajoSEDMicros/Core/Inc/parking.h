04b2e983a5ba79981c7c8b833f71db0033d1540a
Dmeendezz <david.mendez@alumnos.upm.es>
2025-01-18 19:52:50 +0100

    Intento de juntar todo v1

---
+++ TrabajoSEDMicros/Core/Inc/parking.h
@@ -0,0 +1,22 @@
+#ifndef PARKING_H
+#define PARKING_H
+
+#include "stm32f4xx_hal.h"
+
+/* Definiciones de pines */
+#define TRIG_PIN GPIO_PIN_6
+#define TRIG_PORT GPIOA
+#define ECHO_PIN GPIO_PIN_7
+#define ECHO_PORT GPIOA
+
+#define LED_RED_PIN GPIO_PIN_5
+#define LED_RED_PORT GPIOA
+#define LED_GREEN_PIN GPIO_PIN_0
+#define LED_GREEN_PORT GPIOB
+
+/* Prototipos de funciones */
+void Parking_Init(void);
+void Parking_Process(void);
+void Delay_us(uint32_t us);
+
+#endif /* PARKING_H */
