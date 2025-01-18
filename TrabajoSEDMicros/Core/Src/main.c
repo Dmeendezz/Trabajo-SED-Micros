85602ad3449d21c5fcd10f5810bfdb1193a47cef
Dmeendezz <david.mendez@alumnos.upm.es>
2025-01-18 18:05:51 +0100

    ADC + LCD funcional todo en un mismo codigo.

--- TrabajoSEDMicros/Core/Src/main.c
+++ TrabajoSEDMicros/Core/Src/main.c
@@ -18,170 +18,271 @@
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
+#include "lcd.h"
 #include "ADC.h"
-#include <stdlib.h>
-#include <stdio.h>
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
 /* USER CODE END Includes */
+
 /* Private typedef -----------------------------------------------------------*/
 /* USER CODE BEGIN PTD */
 /* USER CODE END PTD */
+
 /* Private define ------------------------------------------------------------*/
 /* USER CODE BEGIN PD */
 /* USER CODE END PD */
+
 /* Private macro -------------------------------------------------------------*/
 /* USER CODE BEGIN PM */
 /* USER CODE END PM */
+
 /* Private variables ---------------------------------------------------------*/
 int16_t adcval;
+int plazas_libres = 4;
+char * plazas_libres_char = "4";
 /* USER CODE BEGIN PV */
+LCD_t lcd;
 /* USER CODE END PV */
+
 /* Private function prototypes -----------------------------------------------*/
 void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
 /* USER CODE BEGIN PFP */
 /* USER CODE END PFP */
+
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
 /* USER CODE END 0 */
+
 /**
-* @brief  The application entry point.
-* @retval int
-*/
+  * @brief  The application entry point.
+  * @retval int
+  */
 int main(void)
 {
-/* USER CODE BEGIN 1 */
-/* USER CODE END 1 */
-/* MCU Configuration--------------------------------------------------------*/
-/* Reset of all peripherals, Initializes the Flash interface and the Systick. */
-HAL_Init();
-/* USER CODE BEGIN Init */
-/* USER CODE END Init */
-/* Configure the system clock */
-SystemClock_Config();
-/* USER CODE BEGIN SysInit */
-/* USER CODE END SysInit */
-/* Initialize all configured peripherals */
-MX_GPIO_Init();
-MX_ADC1_Init(); //Inicializa el ADC desde ADC.c
-/* USER CODE BEGIN 2 */
-/* USER CODE END 2 */
-/* Infinite loop */
-/* USER CODE BEGIN WHILE */
+
+  /* USER CODE BEGIN 1 */
+  /* USER CODE END 1 */
+
+  /* MCU Configuration--------------------------------------------------------*/
+
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init();
+
+  /* USER CODE BEGIN Init */
+  /* USER CODE END Init */
+
+  /* Configure the system clock */
+  SystemClock_Config();
+
+  /* USER CODE BEGIN SysInit */
+  /* USER CODE END SysInit */
+
+  /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  MX_ADC1_Init();
+  /* USER CODE BEGIN 2 */
+  // LCD SETTINGS
+  lcd.RS_port = LCD_RS_GPIO_Port;
+  lcd.RS_pin = LCD_RS_Pin;
+  //lcd.RW_port = LCD_RW_GPIO_Port;
+  //lcd.RW_pin = LCD_RW_Pin;
+  lcd.EN_port = LCD_EN_GPIO_Port;
+  lcd.EN_pin = LCD_EN_Pin;
+  lcd.D4_port = D4_GPIO_Port;
+  lcd.D4_pin = D4_Pin;
+  lcd.D5_port = D5_GPIO_Port;
+  lcd.D5_pin = D5_Pin;
+  lcd.D6_port = D6_GPIO_Port;
+  lcd.D6_pin = D6_Pin;
+  lcd.D7_port = D7_GPIO_Port;
+  lcd.D7_pin = D7_Pin;
+
+  // inicializamos el LCD
+
+  lcd_begin(&lcd, 16, 2, LCD_5x8DOTS);
+
+  /* Escribimos un mensaje inicial */
+/*
+  lcd_setCursor(&lcd, 0, 0);
+  lcd_print(&lcd, "Javier Gasco");
+  lcd_setCursor(&lcd, 0, 1);
+  lcd_print(&lcd, "CaliBeta Prroo!!");
+  HAL_Delay(1500);
+
+  lcd_noDisplay(&lcd); // Apagar el display
+  lcd_display(&lcd);   // Encender el display
+  lcd_clear(&lcd);     // Limpiar el LCD
+  lcd_home(&lcd);      // Llevar el cursor al inicio
+
+  lcd_print(&lcd, "Suscribete");
+  HAL_Delay(1000);
+
+  lcd_cursor(&lcd);    // Mostrar el cursor
+  HAL_Delay(1000);
+
+  lcd_blink(&lcd);     // Hacer parpadear el cursor
+  HAL_Delay(3000);
+
+  lcd_noBlink(&lcd);   // Detener el parpadeo
+  lcd_noCursor(&lcd);  // Ocultar el cursor
+  HAL_Delay(100);
+
+  for (int i = 0; i < 10; ++i) {
+      lcd_scrollDisplayRight(&lcd); // Desplazar texto a la derecha
+      HAL_Delay(300);
+  }
+  for (int i = 0; i < 10; ++i) {
+        lcd_scrollDisplayLeft(&lcd); // Desplazar texto a la izquierda
+        HAL_Delay(300);
+    }
+    */
+  /* USER CODE END 2 */
+
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
 while (1)
 {
-  /* USER CODE END WHILE */
-	 adcval = ADC_Read(); // Lee el valor del ADC
-	 if (adcval > 2048)
-		 HAL_GPIO_WritePin(GPIOD, GPIO_PIN_11, GPIO_PIN_SET); // Enciende LED
-	 else
-		 HAL_GPIO_WritePin(GPIOD, GPIO_PIN_11, GPIO_PIN_RESET); // Apaga LED
-	 HAL_GPIO_WritePin(GPIOD, GPIO_PIN_12, GPIO_PIN_SET);
-  /* USER CODE BEGIN 3 */
+    /* USER CODE END WHILE */
+	adcval = ADC_Read(); // Lee el valor del ADC
+		 if (adcval < 800){
+			 HAL_GPIO_WritePin(GPIOD, GPIO_PIN_11, GPIO_PIN_SET); // Enciende LED
+			 lcd_clear(&lcd);     // Limpiar el LCD
+			 lcd_home(&lcd);      // Llevar el cursor al inicio
+			 lcd_print(&lcd, "Plazas libres: ");
+			 lcd_print(&lcd,plazas_libres_char );
+			 //sprintf(plazas_libres, "%d", plazas_libres_char)
+			 HAL_Delay(1000);
+		 }
+		 else
+			 HAL_GPIO_WritePin(GPIOD, GPIO_PIN_11, GPIO_PIN_RESET); // Apaga LED
+		 HAL_GPIO_WritePin(GPIOD, GPIO_PIN_12, GPIO_PIN_SET);
+
+    /* USER CODE BEGIN 3 */
 }
-/* USER CODE END 3 */
+  /* USER CODE END 3 */
 }
+
 /**
-* @brief System Clock Configuration
-* @retval None
-*/
+  * @brief System Clock Configuration
+  * @retval None
+  */
 void SystemClock_Config(void)
 {
-	 RCC_OscInitTypeDef RCC_OscInitStruct = {0};
-	 RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
-	 /** Configure the main internal regulator output voltage
-	 */
-	 __HAL_RCC_PWR_CLK_ENABLE();
-	 __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
-	 /** Initializes the RCC Oscillators according to the specified parameters
-	 * in the RCC_OscInitTypeDef structure.
-	 */
-	 RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
-	 RCC_OscInitStruct.HSIState = RCC_HSI_ON;
-	 RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
-	 RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
-	 RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
-	 RCC_OscInitStruct.PLL.PLLM = 8;
-	 RCC_OscInitStruct.PLL.PLLN = 50;
-	 RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV4;
-	 RCC_OscInitStruct.PLL.PLLQ = 7;
-	 if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
-	 {
-	   Error_Handler();
-	 }
-	 /** Initializes the CPU, AHB and APB buses clocks
-	 */
-	 RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
-	                             |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
-	 RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
-	 RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
-	 RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV4;
-	 RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV2;
-	 if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_0) != HAL_OK)
-	 {
-	   Error_Handler();
-	 }
-	}
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+
+  /** Configure the main internal regulator output voltage
+  */
+  __HAL_RCC_PWR_CLK_ENABLE();
+  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
+
+  /** Initializes the RCC Oscillators according to the specified parameters
+  * in the RCC_OscInitTypeDef structure.
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
+  RCC_OscInitStruct.PLL.PLLM = 8;
+  RCC_OscInitStruct.PLL.PLLN = 50;
+  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV4;
+  RCC_OscInitStruct.PLL.PLLQ = 7;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Initializes the CPU, AHB and APB buses clocks
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV4;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV2;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_0) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
 /**
-* @brief ADC1 Initialization Function
-* @param None
-* @retval None
-*/
-/**
-* @brief GPIO Initialization Function
-* @param None
-* @retval None
-*/
+  * @brief GPIO Initialization Function
+  * @param None
+  * @retval None
+  */
 static void MX_GPIO_Init(void)
 {
-GPIO_InitTypeDef GPIO_InitStruct = {0};
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
 /* USER CODE BEGIN MX_GPIO_Init_1 */
 /* USER CODE END MX_GPIO_Init_1 */
-/* GPIO Ports Clock Enable */
-__HAL_RCC_GPIOA_CLK_ENABLE();
-__HAL_RCC_GPIOD_CLK_ENABLE();
-/*Configure GPIO pin Output Level */
-HAL_GPIO_WritePin(GPIOD, GPIO_PIN_11 | GPIO_PIN_12, GPIO_PIN_RESET);
-/*Configure GPIO pin : PD11 */
-GPIO_InitStruct.Pin = GPIO_PIN_11 | GPIO_PIN_12;
-GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
-GPIO_InitStruct.Pull = GPIO_NOPULL;
-GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
-HAL_GPIO_Init(GPIOD, &GPIO_InitStruct);
+
+  /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+  __HAL_RCC_GPIOB_CLK_ENABLE();
+  __HAL_RCC_GPIOD_CLK_ENABLE();
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOB, LCD_EN_Pin|LCD_RS_Pin|D7_Pin|D6_Pin
+                          |D5_Pin|D4_Pin, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOD, GPIO_PIN_11|GPIO_PIN_12, GPIO_PIN_RESET);
+
+  /*Configure GPIO pins : LCD_EN_Pin LCD_RS_Pin D7_Pin D6_Pin
+                           D5_Pin D4_Pin */
+  GPIO_InitStruct.Pin = LCD_EN_Pin|LCD_RS_Pin|D7_Pin|D6_Pin
+                          |D5_Pin|D4_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : PD11 PD12 */
+  GPIO_InitStruct.Pin = GPIO_PIN_11|GPIO_PIN_12;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOD, &GPIO_InitStruct);
+
 /* USER CODE BEGIN MX_GPIO_Init_2 */
 /* USER CODE END MX_GPIO_Init_2 */
 }
+
 /* USER CODE BEGIN 4 */
 /* USER CODE END 4 */
+
 /**
-* @brief  This function is executed in case of error occurrence.
-* @retval None
-*/
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
 void Error_Handler(void)
 {
-/* USER CODE BEGIN Error_Handler_Debug */
+  /* USER CODE BEGIN Error_Handler_Debug */
 /* User can add his own implementation to report the HAL error return state */
 __disable_irq();
 while (1)
 {
 }
-/* USER CODE END Error_Handler_Debug */
+  /* USER CODE END Error_Handler_Debug */
 }
+
 #ifdef  USE_FULL_ASSERT
 /**
-* @brief  Reports the name of the source file and the source line number
-*         where the assert_param error has occurred.
-* @param  file: pointer to the source file name
-* @param  line: assert_param error line source number
-* @retval None
-*/
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
 void assert_failed(uint8_t *file, uint32_t line)
 {
-/* USER CODE BEGIN 6 */
+  /* USER CODE BEGIN 6 */
 /* User can add his own implementation to report the file name and line number,
    ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
-/* USER CODE END 6 */
+  /* USER CODE END 6 */
 }
 #endif /* USE_FULL_ASSERT */
-
